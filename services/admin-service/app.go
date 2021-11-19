5a28e47c052eef2fb3f0fac7bbfc3a539b5c6dc3
Eric Mittelhammer <code@ericmittelhammer.com>
2021-11-18 21:06:27 -0500

    instrument admin service

--- services/admin-service/app.go
+++ services/admin-service/app.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"fmt"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/nobuyo/nrfiber"
 )
 
 func main() {
+	nrapp, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("Add Service"),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
+	)
+	if err != nil {
+		fmt.Println("unable to create New Relic Application", err)
+	}
 	// Initialize the application
 	app := fiber.New()
+
+	app.Use(nrfiber.New(nrfiber.Config{
+		NewRelicApp: nrapp,
+	}))
 
 	fmt.Println("Started")
 
@@ -19,7 +33,7 @@ func main() {
 		log.Print("List hats that need moderation")
 
 		var result string
-			
+
 		result = UnmoderatedPic()
 
 		c.Append("Content-Type", "application/json")
--- services/admin-service/go.mod
+++ services/admin-service/go.mod
@@ -5,4 +5,6 @@ go 1.16
 require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gofiber/fiber/v2 v2.15.0
+	github.com/newrelic/go-agent/v3 v3.15.1
+	github.com/nobuyo/nrfiber v0.0.2
 )
